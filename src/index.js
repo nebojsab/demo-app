75165cbc62aa1cf264ab0a71f0655aaf15ae7217
nebojsa <nebojsa.brnjasevic@gmail.com>
2020-06-24 21:46:13 +0200

    Apply mobile styles for landing and form pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import './styles.css'
 import './styles.scss'
 
 ReactDOM.render(<App name=" Nebojsa" />, document.getElementById('app'))
