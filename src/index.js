4025e280a98fd90f6c0883f9d4642a79137e961a
nebojsa <nebojsa.brnjasevic@gmail.com>
2020-09-22 09:51:50 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-import './styles.scss'
-
-ReactDOM.render(<App name=" Nebojsa" />, document.getElementById('app'))
